f769ce6091ee79fd99e709295e265bd6657f5c10
WILSONBAKER <w0441287@campus.nscc.ca>
2023-04-06 17:55:22 -0300

    Added hans files
    Han has already started on the project and has shared this part of his code

---
+++ PROG2500-FinalProject/PROG2500-FinalProject/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PROG2500_FinalProject.Pages;
+
+namespace PROG2500_FinalProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Pages.Home homePage { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            homePage = new Home();
+            mainframe.NavigationService.Navigate(homePage);
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainframe.NavigationService.Navigate(homePage);
+        }
+    }
+}
