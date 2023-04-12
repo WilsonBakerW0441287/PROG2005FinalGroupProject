a57fef493100254cf4375f60adbb84ce5bcb0cdf
WILSONBAKER <w0441287@campus.nscc.ca>
2023-04-12 14:08:29 -0300

    adding buttons to mainwindow for page navigation

--- PROG2500-FinalProject/PROG2500-FinalProject/obj/Debug/net7.0-windows/App.g.cs
+++ PROG2500-FinalProject/PROG2500-FinalProject/obj/Debug/net7.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace PROG2500_FinalProject {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace PROG2500_FinalProject {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public static void Main() {
             PROG2500_FinalProject.App app = new PROG2500_FinalProject.App();
             app.InitializeComponent();
