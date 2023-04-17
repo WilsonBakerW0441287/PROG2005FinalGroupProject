082450d9a60c199ce922dc2f4d8573d971c89e6b
WILSONBAKER <w0441287@campus.nscc.ca>
2023-04-17 16:53:24 -0300

    ide settings

--- PROG2500-FinalProject/PROG2500-FinalProject/obj/Debug/net7.0-windows/App.g.i.cs
+++ PROG2500-FinalProject/PROG2500-FinalProject/obj/Debug/net7.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace PROG2500_FinalProject {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace PROG2500_FinalProject {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/PROG2500-FinalProject;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PROG2500-FinalProject;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace PROG2500_FinalProject {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public static void Main() {
             PROG2500_FinalProject.App app = new PROG2500_FinalProject.App();
             app.InitializeComponent();
--- PROG2500-FinalProject/PROG2500-FinalProject/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ PROG2500-FinalProject/PROG2500-FinalProject/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -95,13 +95,13 @@ namespace PROG2500_FinalProject {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PROG2500-FinalProject;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PROG2500-FinalProject;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -111,7 +111,7 @@ namespace PROG2500_FinalProject {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
