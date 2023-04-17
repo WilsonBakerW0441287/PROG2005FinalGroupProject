5238d54318bde691d795b8ed3503bd9af8eb418b
Abdulrahman Alkasem <43070692+AAlkasem@users.noreply.github.com>
2023-04-17 17:50:55 -0300

    Create Title.cs
    Test Commit

---
+++ PROG2500-FinalProject/PROG2500-FinalProject/Models/Title.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB_Project.Models
+{
+    public partial class Title
+    {
+        public string FormatedTitleCall
+        {
+            get
+            {
+                string adultRating = "";
+                if (IsAdult == true)
+                {
+                    adultRating = "Hide Your Kids!";
+                } else
+                {
+                    adultRating = "For Everyone!";
+                }
+                return string.Format("Title: {0}\nRating: {1}\nYear Of Release: {2}\nRun Time: {3} Minutes",
+                    OriginalTitle,adultRating,StartYear,RuntimeMinutes);
+            }
+        }
+    }
+}
