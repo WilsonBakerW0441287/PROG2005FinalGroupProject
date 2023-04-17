bb28aafa37d1cc0690bf08e026b36053d7c2b86a
WILSONBAKER <w0441287@campus.nscc.ca>
2023-04-17 16:55:21 -0300

    more ide stuff

--- PROG2500-FinalProject/PROG2500-FinalProject/obj/Debug/net7.0-windows/App.g.i.cs
+++ PROG2500-FinalProject/PROG2500-FinalProject/obj/Debug/net7.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace PROG2500_FinalProject {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/PROG2500-FinalProject;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PROG2500-FinalProject;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- PROG2500-FinalProject/PROG2500-FinalProject/obj/Debug/net7.0-windows/Pages/Home.g.i.cs
+++ PROG2500-FinalProject/PROG2500-FinalProject/obj/Debug/net7.0-windows/Pages/Home.g.i.cs
@@ -53,7 +53,7 @@ namespace PROG2500_FinalProject.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PROG2500-FinalProject;V1.0.0.0;component/pages/home.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PROG2500-FinalProject;component/pages/home.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\Home.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
